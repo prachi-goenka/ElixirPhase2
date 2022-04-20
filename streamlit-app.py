54ccbf36234318beda7ca9595e9d6043dba8622e
Prachi Kumari <72649994+prachi-goenka@users.noreply.github.com>
2022-04-20 20:40:12 +0530

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -185,8 +185,6 @@ st.header('Elixir')
 if option=="Home":
     
     #prachi add 
-   with open('style.css')as f:
-     st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
    st.text("Health at your fingertips")
    col1,col2,col3 = st.columns(3)
    with col1:
